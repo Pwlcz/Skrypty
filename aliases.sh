d10aed5f306e16a1fee33aa92d060cfacdd9cf71
Michał Pawlak <thedamnedmadness@gmail.com>
2026-05-16 02:48:08 +0200

    gitignore latex

--- aliases.sh
+++ aliases.sh
@@ -1,2 +1,17 @@
 # source it in ~/.profile, ~/.bashrc, ~/.zshrc, etc.
 alias avup="vagrant up && vagrant ssh-config >> ~/.ssh/config_vscode"
+
+function gitignore-latex() {
+  echo '# LaTeX files
+*.bat
+*.gz
+*.aux
+*.log
+*.bbl
+*.blg
+*.lot
+*.tcp
+*.toc
+*.lof
+# End of LaTeX files' >> .gitignore
+}
